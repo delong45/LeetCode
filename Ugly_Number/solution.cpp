83fb2939fde7961e413a06b01796a9f08f124f8f
delong <fsdelong45@163.com>
2016-03-11 19:04:31 +0800

    Ugly Number

---
+++ Ugly_Number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num < 1) return false;
+        if (num == 1) return true;
+
+        while (num >= 2 && num % 2 == 0) num /= 2;
+        while (num >= 3 && num % 3 == 0) num /= 3;
+        while (num >= 5 && num % 5 == 0) num /= 5;
+
+        return (num == 1);
+    }
+};
