940bf4cb7ae88ec24c2f0cf49013db9f6e0fd859
delong <fsdelong45@163.com>
2016-03-19 23:35:27 +0800

    Reverse Linked List

---
+++ Reverse_Linked_List/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+
+        while (head != NULL) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+};
