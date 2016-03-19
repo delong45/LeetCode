8dd9e367759c18bd9754f68b12be3cba6f176c92
delong <fsdelong45@163.com>
2016-03-19 17:43:14 +0800

    House Robber II

---
+++ House_Robber_II/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        std::vector<int> dp(n,0);
+        dp[0] = nums[0];
+        dp[1] = std::max(nums[0],nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = std::max(nums[i]+dp[i-2], dp[i-1]);
+        }
+        return dp.back();     
+    }
+};
